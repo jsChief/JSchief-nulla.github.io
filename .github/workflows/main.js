cb1e88a3dd8d069e3e55e611eafb27b2c1b5b327
jsChief <96917732+jsChief@users.noreply.github.com>
2025-09-01 15:09:25 +0100

    Added files via upload

---
+++ .github/workflows/main.js
@@ -0,0 +1,18 @@
+var vue = new Vue({
+el: "#app",
+mounted(){
+  let k = document.createElement('div');
+  k.className = "flower";
+  k.textContent = "⛺";
+  document.body.appendChild(k);
+},
+data: {
+
+},
+computed: {
+
+},
+methods: {
+
+}
+});
